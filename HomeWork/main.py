be93c4e5ba54318c0ac65c8bb73637c1f49399fc
RemoveFire <siv.vitya@yandex.ru>
2022-11-24 10:40:28 +0300

    up file main

--- HomeWork/main.py
+++ HomeWork/main.py
@@ -3,8 +3,8 @@ from Unit1.functions import NumberTask
 def HomeWork():
     NumberTask()
 def main():
+    # Выполняем команду выполнения задач из урока 1
+    print('\n\t\tВыполняем домашнее задание №1')
     HomeWork()
-
-# Выполняем команду выполнения задач из урока 1
-print('\n\t\tВыполняем домашнее задание №1')
+# Запускаем главную функцию
 main()
