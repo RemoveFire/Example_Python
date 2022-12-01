55f3d4ffc741f4ea7c14f8f7fb386ef67a027ac8
RemoveFire <siv.vitya@yandex.ru>
2022-12-01 19:25:53 +0300

    updated files and added 2 task to unit 2

--- HomeWork/Unit2/TaskUnit2.py
+++ HomeWork/Unit2/TaskUnit2.py
@@ -24,7 +24,12 @@ def U2Task1():
 
 def U2Task2():
     print("Решение задачи №2: \n")
+    from math import factorial
 
+    N = int(input_check("Введите число: "))
+    fun = lambda x: ((x == 1) and 1) or x * factorial(x - 1)
+    list2 = list(fun(i) for i in range(1, N + 1))
+    print(f"\nНабор произведений чисел от 1 до {N}:  {list2}")
 
 
 def U2Task3():
