f160f01b0fcb2ce62eb17e2d8790c47e0e4af3bd
RemoveFire <siv.vitya@yandex.ru>
2022-12-13 21:12:44 +0300

    Added Task 3 in the file TaskUnit5.py

--- HomeWork/Unit5/TaskUnit5.py
+++ HomeWork/Unit5/TaskUnit5.py
@@ -46,8 +46,6 @@ def U5Task1():
 def U5Task2():
     print("Решение задачи №2: \n")
 
-
-
     messages = ['Ваш ход брать конфеты', 'Возьмите конфеты',
                 'сколько конфет берем?', 'берите еще', 'Ваш ход']
     max_number_move = 0
@@ -126,8 +124,183 @@ def U5Task2():
         print(f'Поздравляю! В этот раз победил {winer}! '
               f'Ему достаются все конфеты!')
 
+
 def U5Task3():
     print("Решение задачи №3: \n")
+
+    # Инициализация карты
+    maps = [1, 2, 3,
+            4, 5, 6,
+            7, 8, 9]
+
+    # Инициализация победных линий
+    victories = [[0, 1, 2],
+                 [3, 4, 5],
+                 [6, 7, 8],
+                 [0, 3, 6],
+                 [1, 4, 7],
+                 [2, 5, 8],
+                 [0, 4, 8],
+                 [2, 4, 6]]
+
+    # Вывод карты на экран
+    def print_maps():
+        print("\n")
+        print("\t     |     |")
+        print("\t  {}  |  {}  |  {}".format(maps[0], maps[1], maps[2]))
+        print('\t_____|_____|_____')
+
+        print("\t     |     |")
+        print("\t  {}  |  {}  |  {}".format(maps[3], maps[4], maps[5]))
+        print('\t_____|_____|_____')
+
+        print("\t     |     |")
+
+        print("\t  {}  |  {}  |  {}".format(maps[6], maps[7], maps[8]))
+        print("\t     |     |")
+        print("\n")
+
+    # Сделать ход в ячейку
+    def step_maps(step, symbol):
+        ind = maps.index(step)
+        maps[ind] = symbol
+
+    # Получить текущий результат игры
+    def get_result():
+        win = ""
+        for i in victories:
+            if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
+                win = "X"
+            if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
+                win = "O"
+        return win
+
+    # Искусственный интеллект: поиск линии с нужным количеством X и O на победных линиях
+    def check_line(sum_O, sum_X):
+
+        step = ""
+        for line in victories:
+            o = 0
+            x = 0
+
+            for j in range(0, 3):
+                if maps[line[j]] == "O":
+                    o = o + 1
+                if maps[line[j]] == "X":
+                    x = x + 1
+
+            if o == sum_O and x == sum_X:
+                for j in range(0, 3):
+                    if maps[line[j]] != "O" and maps[line[j]] != "X":
+                        step = maps[line[j]]
+
+        return step
+
+    # Искусственный интеллект: выбор хода
+    def AI():
+
+        step = ""
+
+        # 1) если на какой-либо из победных линий 2 свои фигуры и 0 чужих - ставим
+        step = check_line(2, 0)
+
+        # 2) если на какой-либо из победных линий 2 чужие фигуры и 0 своих - ставим
+        if step == "":
+            step = check_line(0, 2)
+
+            # 3) если 1 фигура своя и 0 чужих - ставим
+        if step == "":
+            step = check_line(1, 0)
+
+            # 4) центр пуст, то занимаем центр
+        if step == "":
+            if maps[4] != "X" and maps[4] != "O":
+                step = 5
+
+                # 5) если центр занят, то занимаем первую ячейку
+        if step == "":
+            if maps[0] != "X" and maps[0] != "O":
+                step = 1
+
+        return step
+
+    def player_human():
+        human = True
+        FirstPlayer = input("Первый Игрок.  Введите Ваше имя: ")
+        print("\n")
+        SecondPlayer = input("Второй Игрок. Введите Ваше имя: ")
+        print("\n")
+        game_over = False
+        while game_over == False:
+
+            # 1. Показываем карту
+            print_maps()
+
+            # 2. Спросим у играющего куда делать ход
+            if human == True:
+                symbol = "X"
+                step = int(input(f"{FirstPlayer}, ваш ход: "))
+            else:
+                symbol = "O"
+                step = int(input(f"{SecondPlayer}, ваш ход: "))
+
+            step_maps(step, symbol)  # делаем ход в указанную ячейку
+            win = get_result()  # определим победителя
+            if win != "":
+                game_over = True
+            else:
+                game_over = False
+
+            human = not (human)
+        # Игра окончена. Покажем карту. Объявим победителя.
+        print_maps()
+        print("Победил", win)
+
+    def player_bot():
+        print("Играем с Компом")
+        game_over = False
+        BOT = True
+
+        while game_over == False:
+
+            # 1. Показываем карту
+            print_maps()
+
+            # 2. Спросим у играющего куда делать ход
+            if BOT == True:
+                symbol = "X"
+                step = int(input("Человек, ваш ход: "))
+            else:
+                print("Компьютер делает ход: ")
+                symbol = "O"
+                step = AI()
+
+            # 3. Если компьютер нашел куда сделать ход, то играем. Если нет, то ничья.
+            if step != "":
+                step_maps(step, symbol)  # делаем ход в указанную ячейку
+                win = get_result()  # определим победителя
+                if win != "":
+                    game_over = True
+                else:
+                    game_over = False
+            else:
+                print("Ничья!")
+                game_over = True
+                win = "дружба"
+
+            BOT = not (BOT)
+
+        # Игра окончена. Покажем карту. Объявим победителя.
+        print_maps()
+        print("Победил", win)
+
+    # Основная программа
+
+    WHO = input("Как играем?\n - Если между собой введи 1\n - Если с компьютером введи 2\n - Ввод: ")
+    if WHO == "1":
+        player_human()
+    elif WHO == "2":
+        player_bot()
 
 
 def U5Task4():
