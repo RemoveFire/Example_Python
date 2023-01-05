4839f5458c1cd40a2e274dd8de8184ec66d2dd26
RemoveFire <siv.vitya@yandex.ru>
2023-01-05 16:20:54 +0300

    added cloud texture - objectProperty

--- Platform_Game/src/main.py
+++ Platform_Game/src/main.py
@@ -1,8 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
 class Background(Widget):
+    cloud_texture = ObjectProperty(None)
+
+
     pass
 
 
