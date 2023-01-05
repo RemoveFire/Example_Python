fd47f78a9ee03985c2f78e9f11f38e646e6c37e8
RemoveFire <siv.vitya@yandex.ru>
2023-01-05 16:27:39 +0300

    create cloud texture

--- Platform_Game/src/main.py
+++ Platform_Game/src/main.py
@@ -1,10 +1,18 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.uix.image import Image
+
 
 
 class Background(Widget):
     cloud_texture = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Create textures / Создание тексутр
+        self.cloud_texture = Image(source="cloud.png").texture
 
 
     pass
